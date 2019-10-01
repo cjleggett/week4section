13f40fc051ea6beb8513f7ef66cb9c00dfa7cbc5
Connor Leggett <43161915+cjleggett@users.noreply.github.com>
2019-10-01 15:52:46 -0400

    Create file_practice.c

---
+++ file_practice.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
+        printf("Usage: ./file_practice infile copy_outfile capitalized_outfile\n");
+        return 1;
+    }
+
+}
